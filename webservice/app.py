729f813355727128006a0009746d86f39e687932
Karol Adameczek <karol24999@gmail.com>
2021-01-11 18:26:06 +0100

    Created initial files for web service

---
+++ webservice/app.py
@@ -0,0 +1,15 @@
+from flask import Flask, render_template
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+    return render_template("index.html")
+
+@app.route("/generate")
+def generate():
+    return {
+        "message" : "Nothing here yet ¯\\_(ツ)_/¯"
+    }
+
+if __name__ == "__main__":
+    app.run()
