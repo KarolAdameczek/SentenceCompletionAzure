308bec67655603f114f2ff406e722fcdfaf8765d
Karol Adameczek <karol24999@gmail.com>
2021-01-21 11:03:50 +0100

    Added JS functions, which request new sentences, created simple mock in WS to be replaced with request to ML model in future

--- webservice/app.py
+++ webservice/app.py
@@ -1,15 +1,31 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
+import requests
 app = Flask(__name__)
+
+def getSentences(text):
+    # to be implemented
+    # send request to the ML model for new sentences
+
+    sentences = []
+
+    for i in range(10):
+        sentences.append(text + " " + str(i))
+
+    return sentences
 
 @app.route("/")
 def hello():
     return render_template("index.html")
 
-@app.route("/generate")
+@app.route("/generate", methods=["POST"])
 def generate():
-    return {
-        "message" : "Nothing here yet ¯\\_(ツ)_/¯"
-    }
+    try:
+        json = request.get_json()
+        text = json["text"]
+    except:
+        return ""
+
+    return {"sentences" : getSentences(text)}
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run()
--- webservice/static/main.js
+++ webservice/static/main.js
@@ -2,23 +2,43 @@ input               = document.getElementById("text-input")
 sentence_container  = document.getElementById("sentence-containter")
 sentence_table_body = document.getElementById("sentence-table-body")
 
-sentences = [
-    "Ala ma kota",
-    "Ula ma psa",
-    "Lorem ipsum",
-    "Przykładowe zdanie"
-]
+var sentences = []
 
-input.oninput = function(e) {
+input.oninput = onInputChange
+
+function onInputChange(e){
     if (input.value) {
-        if(sentence_container.style.display === ""){
+        if(sentences.length == 0){
+            getSentences(input.value)
+            return
+        }
+        if(!sentences.every(item => {
+            return item.startsWith(input.value)
+        })){
+            getSentences(input.value)
+        }
+    } else {
+        clearNode(sentence_table_body)
+        sentences = []
+    }
+}
+
+function getSentences(text){
+    var xhr = new XMLHttpRequest();
+    xhr.open("POST", "/generate");
+    xhr.setRequestHeader("Content-Type", "application/json;charset=UTF-8");
+    xhr.responseType = "json";
+    xhr.onload = function() {
+        if(xhr.status == 200){
+            sentences = xhr.response.sentences;
+
+            clearNode(sentence_table_body);
             populateTable(sentences);
         }
-        sentence_container.style.display = "block";
-      } else {
-        sentence_container.style.display = "";
-        clearNode(sentence_table_body)
-      }
+    };
+    xhr.send(JSON.stringify({
+        "text" : text
+    }));
 }
 
 function clearNode(node){
@@ -35,6 +55,7 @@ function populateTable(list){
         tr.appendChild(td)
         tr.onclick = (function(e){
             input.value = e.target.innerText
+            onInputChange()
         })
         sentence_table_body.appendChild(tr)
     })
